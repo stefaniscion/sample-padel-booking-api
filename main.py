25cb878e037d0bae30c4df82e0a22a4c78b2916a
Stefano Di Pasquale <stefaniscion@gmail.com>
2023-10-02 11:29:40 +0200

    added create_match

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import time
 from fastapi import FastAPI
+
 #connection to database
 from db_connection import engine, db_connection
 from sqlalchemy import select, insert
@@ -21,3 +23,31 @@ def get_match(match_id: int):
             return row[0]
 
 
+@app.post("/match")
+def create_match(date: str, slot: int):
+    #check if input is valid
+    if slot < 9 or slot > 23:
+        return {"message": "Invalid slot"}
+    try:
+        date_date = time.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return {"message": "Invalid date"}
+    #check if match already exists
+    stmt = select(Match).where(Match.date == date, Match.slot == slot)
+    with Session(engine) as session:
+        result =  session.execute(stmt).first()
+        print(result)
+        if result is not None:
+            return {"message": "Match already exists"}
+    #then create match
+    stmt = insert(Match).values(date=date, slot=slot)
+    with Session(engine) as session:
+        session.execute(stmt)
+        session.commit()
+        return {"message": "Match created"}
+
+    
+#@app.get("/book_match/{match_id}/{user_id}/{slot_id}")
+#def book_match(match_id:int, user_id: int, slot_id: int):
+#    pass
+#     
